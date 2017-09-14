69c6ab876c258bfe22163faf1c6230b2e3432564
Lunatic Clark <13760798@qq.com>
2017-09-14 13:18:50 +0800

    新增mock server服务以及代理访问数据服务功能

--- README.md
+++ README.md
@@ -14,17 +14,8 @@ npm run dev
 # build for production with minification
 npm run build
 
-# build for production and view the bundle analyzer report
-npm run build --report
+#启动静态mock server 127.0.0.1:3000
+npm run mock 
 
-# run unit tests
-npm run unit
-
-# run e2e tests
-npm run e2e
-
-# run all tests
-npm test
-```
 
 For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -37,13 +37,14 @@
     </ul>
     <ul>
       <li v-for="article in articles">
-        {{article.title}}
+        {{article.name}}
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { getArticles } from '../api/api';
 export default {
   name: 'hello',
   data () {
@@ -53,17 +54,10 @@ export default {
     }
   },
   mounted: function () {
-    this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10', {}, {
-      headers: {
-      },
-      emulateJSON: true
-    }).then(function (response) {
-      // 这里是处理正确的回调
-      this.articles = response.data.subjects
+    getArticles().then(response => {
+      debugger;
+      this.articles = response.data.body
       // this.articles = response.data["subjects"] 也可以
-    }, function (response) {
-      // 这里是处理错误的回调
-      console.log(response)
     })
   }
 }
