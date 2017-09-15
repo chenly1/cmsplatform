3f9e32a563cc04af7e2e2faeb817e0f867a829eb
Holiday <leijing_zhang@163.com>
2017-09-15 14:03:34 +0800

    解决页面css与js报错

--- src/components/ue/ue.vue
+++ src/components/ue/ue.vue
@@ -12,9 +12,6 @@
       }
     },
     props: {
-      defaultMsg: {
-        type: String
-      },
       config: {
         type: Object
       },
@@ -25,9 +22,6 @@
     mounted() {     
       const _this = this;
       this.editor = UE.getEditor(this.id, this.config); // 初始化UE
-      this.editor.addListener("ready", function () {
-        _this.editor.setContent(_this.defaultMsg); // 确保UE加载完成后，放入内容。
-      });
     },
     methods: {
       getUEContent() { // 获取内容方法
