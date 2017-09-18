f5b07c21b16ac1f83ae60b5cdc9dc1eea468fd00
Lunatic Clark <13760798@qq.com>
2017-09-18 13:41:08 +0800

    修改图片上传代理问题

--- config/proxyConfig.js
+++ config/proxyConfig.js
@@ -1,17 +1,25 @@
 module.exports = {
-    proxyList: {
-          '/apis': {
-              // 测试环境
-              target: 'http://127.0.0.1:3000',  // 接口域名
-              changeOrigin: true,  //是否跨域
-              pathRewrite: {
-                  '^/apis': ''   //需要rewrite重写的,
-              }
-            },
-              '/net': {
-                // 测试环境
-                target: 'http://192.168.0.249:2105/Content/Ueditor/',  // 接口域名
-                changeOrigin: true  //是否跨域        
-          }
+  proxyList: {
+    '/apis': {
+      // 测试环境
+      target: 'http://127.0.0.1:3000',  // 接口域名
+      changeOrigin: true,  //是否跨域
+      pathRewrite: {
+        '^/apis': ''   //需要rewrite重写的,
+      }
+    },
+    '/static/UE/net/controller.ashx': {
+      target: 'http://192.168.0.249:2105/Content/Ueditor/net/controller.ashx',  // 接口域名
+      changeOrigin: true  //是否跨域        
+    },
+    '/Content/ueditor/net/':{
+      target:'http://192.168.0.249:2105/Content/ueditor/net/',
+      changeOrigin:true,
+      router: function(req) {
+        return 'http://192.168.0.249:2105/Content/ueditor/net/';
     }
+    }
+    
+
   }
+}
