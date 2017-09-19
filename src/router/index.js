7b4c5ccf41310eef4642eb84bf511036b972586d
Holiday <leijing_zhang@163.com>
2017-09-19 16:54:16 +0800

    表单提交

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import NotFound from '@/views/404.vue'
 
 import ModelTable from '@/views/nav1/ModelTable.vue'
 import Material from '@/views/nav1/Material.vue'
+import Form from '@/views/Form.vue'
 import ue from '@/views/editor.vue'
 
 Vue.use(Router)
@@ -19,26 +20,27 @@ export default new Router({
       hidden: true
     },
     {
-        path: '/',
-        component: Home,
-        name: '管理系统',
-        iconCls: 'fa fa-th-large',
-        leaf: true,//只有一个节点
-        redirect:'/hello',
-        children: [
-            { path: '/hello', component: Hello, name: '主页' }
-        ]
+      path: '/',
+      component: Home,
+      name: '管理系统',
+      iconCls: 'fa fa-th-large',
+      leaf: true, // 只有一个节点
+      redirect: '/hello',
+      children: [
+        { path: '/hello', component: Hello, name: '主页' }
+      ]
     },
     {
-        path: '/',
-        component: Home,
-        name: '导航一',
-        iconCls: 'fa fa-subway',//图标样式class
-        children: [
-          { path: '/modelTable', component: ModelTable, name: '模版消息' },
-          { path: '/material', component: Material, name: '素材管理' },
-          { path: '/editor', component: ue, name: 'editor', class: 'fa-plug' }
-        ]
+      path: '/',
+      component: Home,
+      name: '导航一',
+      iconCls: 'fa fa-subway', // 图标样式class
+      children: [
+        { path: '/modelTable', component: ModelTable, name: '模版消息' },
+        { path: '/material', component: Material, name: '素材管理' },
+        { path: '/form', component: Form, name: '表单' },
+        { path: '/editor', component: ue, name: 'editor', class: 'fa-plug' }
+      ]
     },
     {
       path: '*',
